0302a7ab72dd30815efc393d7caf4421532b0147
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2021-02-09 15:36:50 +0700

    tungnt

--- root/assets/js/common.js
+++ root/assets/js/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $(".c-header__toggle").click(function(){
         $(this).toggleClass('is-active');
+        $(this).parents().find('.c-nav').toggleClass('is-active');
     });
 });
