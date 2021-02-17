652dff6f59ec554c1879ffd6cd1914bbe3e60da7
tunngnt <tungnt@icd-vn.com>
2021-02-17 10:46:30 +0700

    tungnt'

--- root/assets/js/common.js
+++ root/assets/js/common.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-    $(".c-header__toggle").click(function(){
+    $(".c-header__toggle").click(function() {
         $(this).toggleClass('is-active');
         $(this).parents().find('.c-nav').toggleClass('is-active');
+    });
+    $(".is-child").click(function() {
+        $(this).parents().find('.sub-menu').toggleClass('is-active');
     });
 });
